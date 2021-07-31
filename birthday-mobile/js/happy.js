5a6a01744864776b9caf031df8b1dd27d1dfd7bd
创维俱乐部 <skyworthclub@chuangweijulebudeMacBook-Pro.local>
2021-07-31 14:12:38 +0800

    test mobile demo

--- birthday-mobile/js/happy.js
+++ birthday-mobile/js/happy.js
@@ -1,30 +1,32 @@
 $(function () {
     setTimeout(function () {
         $('.name').animate({
-            opacity:"1",
-            top:"15%"
-        },2000);
+            opacity: "1",
+            top: "15%"
+        }, 2000);
 
-    },6000);
+    }, 6000);
     setTimeout(function () {
         $('.happy').animate({
-            opacity:"1",
-            top:"15%"
-        },2000);
+            opacity: "1",
+            top: "15%"
+        }, 2000);
 
-    },6000);
+    }, 6000);
     setTimeout(function () {
         $('.button-style1').animate({
-            opacity:"1",
-            top:"70%"
-        },1500);
+            opacity: "1",
+            top: "70%"
+        }, 1500);
         $('.button-style2').animate({
-            opacity:"1",
-            top:"85%"
-        },2000);
-    },9000);
-	// $(window).click(function(){
-	// 	$('audio')[0].play();
-	// });
-	// window.audio=$('audio')[0].play();
+            opacity: "1",
+            top: "85%"
+        }, 2000);
+    }, 9000);
+
+    // 只能用户操作后才能播放音频
+    $(window).click(function () {
+        $('audio')[0].play();
+    });
+    // window.audio = $('audio')[0].play();
 })
